cf3bb313bbee17db7fa10dfea719c29d3b7e0090
Sergey <sergokulik61@gmail.com>
2025-05-16 15:19:40 +0400

    Init

---
+++ src/main/java/com/example/T1_starter/T1StarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.T1_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1StarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/T1_starter/T1StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.T1_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
